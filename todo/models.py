c1ee6167f819b3a07d768bedc701cd7f692a08f7
Dom-888 <spinelladomenico88@gmail.com>
2020-06-17 15:17:31 +0000

    Add first class

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BolleanField(null=False, blank=False, default=False)
