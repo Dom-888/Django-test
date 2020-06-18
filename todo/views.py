df06912dc663fc3f1263b218cec83a4ace3835ae
Dom-888 <spinelladomenico88@gmail.com>
2020-06-18 09:11:53 +0000

    Render Item model

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
 
 # Create your views here.
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all() # Contains the whole table
+    context = { # Separate the teble object in a dictionary
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
