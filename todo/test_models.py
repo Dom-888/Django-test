487c1b83c6cf8782559924237f3e5686ce9c913c
Dom-888 <spinelladomenico88@gmail.com>
2020-06-23 17:13:58 +0000

    Add test coverage and complete all the tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 from .models import Item 
 
-# Crea un istanza di item fittizia per verificare my_item.done = "False", ovvero la condizione di default
-
 class TestModels(TestCase):
+
+    # Crea un istanza di item fittizia per verificare my_item.done = "False", ovvero la condizione di default
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name="my_item")
         self.assertFalse(item.done)
+
+    # Questo test è stato aggiunto per completare la coverage. Verifica che lo str(my_item) ritorna il suo nome (e quindi?)
+    def test_item_string_method_return_name(self):
+        item = Item.objects.create(name='my_item')
+        self.assertEqual(str(item), 'my_item')
